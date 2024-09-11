ccc2f160c8ff7c25ea6d59d2a488d5c4edced102
Nur <nur@example.com>
2024-09-11 11:09:06 +0500

    verstka-polni_and_zadanie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,70 @@
+import { NgFor,NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+
+const newPages: number[]=[5,4,3,2,1]
+
+const menuItems:string[]= ['Каталог','Стройматериалы', 'Инструменты', 'Электрика','Интерьер и одежда' ]
+
+const upperCaseMenuItems:string[] = menuItems.map(
+  (item:string)=> {
+    return item.toUpperCase();
+  }
+)
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet,NgIf,NgFor],
+  templateUrl:'./app.component.html',
+  styleUrl:'./app.component.scss'
 })
-export class AppComponent {
+
+
+
+export class AppComponent { 
   title = 'mentoring-first-project';
+
+  isShowMujik = true;
+
+  isShowCatalog = true;
+
+  readonly headerItem = 'Главная';
+
+  readonly headerItem2 ='О компании';
+
+   headerItem3 = 'Каталог';
+
+
+  readonly header2Item1 ='Каталог';
+
+  readonly header2Item2 ='Стройматериалы';
+
+  readonly header2Item3 ='Инструменты';
+ 
+  readonly header2Item4 ='Электрика';
+
+  readonly header2Item5 ='Интерьер и одежда';
+
+  readonly newPages: number[]= newPages;
+
+  menuItems:string[] = upperCaseMenuItems;
+
+  isUpperCase:boolean = true;
+
+
+  changeMenuText():void {
+    this.menuItems = upperCaseMenuItems.map(
+    item => this.isUpperCase ? item.toLowerCase():item.toUpperCase()
+  
+    )
+  
+    this.isUpperCase =!this.isUpperCase
+  }
 }
+
+
+
+
